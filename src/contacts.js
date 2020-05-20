8b3b37d7a643fec4326eebbac9afbe60d93d02a3
Elena Kashtanova <lenachestnut@gmail.com>
2020-05-20 16:12:35 +0300

    Add contact page

---
+++ src/contacts.js
@@ -0,0 +1,24 @@
+const contactPage = function() {
+    const content = document.querySelector("#content");
+
+    const title = document.createElement('h1');
+    title.textContent = 'Green Dragon';
+    content.appendChild(title);
+
+    const address = document.createElement('div');
+
+    address.innerHTML = "<p>Find us at: <br><br>Bywater on the Bywater Road, <br>the building nearest to Hobbiton, <br>one mile south-east from the bridge over the Water that leads to Bag End.</p>";
+    address.style.maxWidth = '300px';
+    address.style.margin = '0 auto';
+    content.appendChild(address);
+
+    const map = document.createElement('img');
+    map.setAttribute('src', 'https://i.pinimg.com/originals/17/66/71/1766719ba90c075651066ec6a54b8c21.jpg');
+    map.style.width = '90%';
+    map.style.maxWidth = '800px';
+    map.style.display = 'inline-block';
+    map.style.marginBottom = '3rem';
+    content.appendChild(map);
+}
+
+export default contactPage
